36d28ce815fbad8b30aa647df944a1c1fd4ce769
tanishkasachdeva <73342817+tanishkasachdeva@users.noreply.github.com>
2023-07-18 17:13:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -16,6 +16,97 @@ class Paper {
   rotating = false;
 
   init(paper) {
+    document.addEventListener('touchmove', (e) => {
+      e.preventDefault(); // Prevent scrolling on touch move
+      
+      const touch = e.touches[0];
+      if (!this.rotating) {
+        this.mouseX = touch.clientX;
+        this.mouseY = touch.clientY;
+
+        this.velX = this.mouseX - this.prevMouseX;
+        this.velY = this.mouseY - this.prevMouseY;
+      }
+
+      const dirX = touch.clientX - this.mouseTouchX;
+      const dirY = touch.clientY - this.mouseTouchY;
+      const dirLength = Math.sqrt(dirX * dirX + dirY * dirY);
+      const dirNormalizedX = dirX / dirLength;
+      const dirNormalizedY = dirY / dirLength;
+
+      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
+      let degrees = (180 * angle) / Math.PI;
+      degrees = (360 + Math.round(degrees)) % 360;
+      if (this.rotating) {
+        this.rotation = degrees;
+      }
+
+      if (this.holdingPaper) {
+        if (!this.rotating) {
+          this.currentPaperX += this.velX;
+          this.currentPaperY += this.velY;
+        }
+        this.prevMouseX = this.mouseX;
+        this.prevMouseY = this.mouseY;
+
+        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
+      }
+    });
+
+    paper.addEventListener('touchstart', (e) => {
+      const touch = e.touches[0];
+      if (this.holdingPaper) return;
+      this.holdingPaper = true;
+
+      paper.style.zIndex = highestZ;
+      highestZ += 1;
+
+      if (e.touches.length === 1) {
+        this.mouseTouchX = touch.clientX;
+        this.mouseTouchY = touch.clientY;
+        this.prevMouseX = touch.clientX;
+        this.prevMouseY = touch.clientY;
+      }
+      if (e.touches.length === 2) {
+        this.rotating = true;
+      }
+    });
+
+    window.addEventListener('touchend', () => {
+      this.holdingPaper = false;
+      this.rotating = false;
+    });
+  }
+}
+
+const papers = Array.from(document.querySelectorAll('.paper'));
+
+papers.forEach(paper => {
+  const p = new Paper();
+  p.init(paper);
+});
+
+
+
+
+/*let highestZ = 1;
+
+class Paper {
+  holdingPaper = false;
+  mouseTouchX = 0;
+  mouseTouchY = 0;
+  mouseX = 0;
+  mouseY = 0;
+  prevMouseX = 0;
+  prevMouseY = 0;
+  velX = 0;
+  velY = 0;
+  rotation = Math.random() * 30 - 15;
+  currentPaperX = 0;
+  currentPaperY = 0;
+  rotating = false;
+
+  init(paper) {
     document.addEventListener('mousemove', (e) => {
       if(!this.rotating) {
         this.mouseX = e.clientX;
@@ -79,4 +170,4 @@ const papers = Array.from(document.querySelectorAll('.paper'));
 papers.forEach(paper => {
   const p = new Paper();
   p.init(paper);
-});
+});*/
